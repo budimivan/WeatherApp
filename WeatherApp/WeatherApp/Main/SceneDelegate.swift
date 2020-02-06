47e1da2f67c2511e014072262dfb7afa28e2001b
budimivan <budim.ivan@gmail.com>
2020-02-06 14:09:45 +0100

    Aligning line 15 in SceneDelegate

--- WeatherApp/WeatherApp/Main/SceneDelegate.swift
+++ WeatherApp/WeatherApp/Main/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let navigationService = NavigationService()
-            navigationService.pushWeatherViewController(window: self.window!)
+        navigationService.pushWeatherViewController(window: self.window!)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
