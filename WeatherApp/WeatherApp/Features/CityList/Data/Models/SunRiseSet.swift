748f32b162a134bedd3f919e6c9d3f42235d8467
budimivan <budim.ivan@gmail.com>
2020-02-10 15:17:16 +0100

    Update SunRiseSet struct

--- WeatherApp/WeatherApp/Features/CityList/Data/Models/SunRiseSet.swift
+++ WeatherApp/WeatherApp/Features/CityList/Data/Models/SunRiseSet.swift
@@ -9,12 +9,4 @@ struct SunRiseSet: Decodable {
         case sunRise = "sunrise"
         case sunSet = "sunset"
     }
-    
-//    do I need this block of code?
-    init(from decoder: Decoder) throws {
-        
-        let valueContainer = try decoder.container(keyedBy: CodingKeys.self)
-        self.sunRise = try valueContainer.decode(Double.self, forKey: CodingKeys.sunRise)
-        self.sunSet = try valueContainer.decode(Double.self, forKey: CodingKeys.sunSet)
-    }
 }
